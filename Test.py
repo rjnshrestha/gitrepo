76054507e7720b114374ffbde3d72976a67a704f
RJ Shrestha <shresth.rj@gmail.com>
2020-09-04 11:47:57 -0400

    Test.py

--- Test.py
+++ Test.py
@@ -1,1 +1,10 @@
-print("Hello")
+import boto3
+
+# Retrieve the list of existing buckets
+s3 = boto3.client('s3')
+response = s3.list_buckets()
+
+# Output the bucket names
+print('Existing buckets:')
+for bucket in response['Buckets']:
+    print(f'  {bucket["Name"]}')
